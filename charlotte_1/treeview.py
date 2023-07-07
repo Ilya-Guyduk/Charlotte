bcf4657844d0b51c89404ad9dc78390468b25e0e
Илья Гайдук / Ilya Gayduk <99331710+Ilya-Gayduk@users.noreply.github.com>
2023-07-07 18:36:07 +0700

    начинаю пилить менюшку с серверами

--- charlotte_1/treeview.py
+++ charlotte_1/treeview.py
@@ -1,15 +1,33 @@
 from tkinter import *
 from tkinter import ttk
+import sqlite3
+
 
 
 
 def treeview(parent):
+
+	conn = sqlite3.connect('charlotte')
+	cursor = conn.cursor()
+
+	# Получить данные из таблицы servers
+	cursor.execute("SELECT alias FROM servers")
+	rows = cursor.fetchall()
+	conn.close()
+
+
 	tree = ttk.Treeview(parent, show="tree")
-	tree.grid(sticky="nsew")	
+	tree.grid(sticky="nsew")
 
-	# добавляем отделы
-	tree.insert("", END, iid=1, text="192.168.10.1")
+	# Полученные данные из таблицы servers
+	for i, row in enumerate(rows):
+		alias = row[0]
+		iid = i + 1
 
-	tree.insert(1, index=END, text="Мониторинг")
-	tree.insert(1, index=END, text="Ностройки")
-	tree.insert(1, index=END, text="Пользователь")
+		# Вставить строки в ttk.Treeview
+		tree.insert("", END, iid=iid, text=alias)
+
+
+	#tree.insert(1, index=END, text="Мониторинг")
+	#tree.insert(1, index=END, text="Ностройки")
+	#tree.insert(1, index=END, text="Пользователь")
